f98488199c3f70bbababec3dc4cd1db34cb870ae
Bilthon <bilthon@gmail.com>
2026-07-01 00:02:27 -0500

    style: add pointer cursor to enabled buttons

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -6,7 +6,7 @@ interface PaginationProps {
 
 const iconProps = {
   viewBox: '0 0 20 20',
-  className: 'h-4 w-4',
+  className: 'pointer-events-none h-4 w-4',
   fill: 'none',
   stroke: 'currentColor',
   strokeWidth: 2,
